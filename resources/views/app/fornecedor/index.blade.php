92bd7f27e004443b7b840d3673d4e86b6911109c
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2021-12-29 16:58:45 -0300

    commit default

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,5 +1,12 @@
 <h3>Fornecedores</h3>
 
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[1]['nome']}}
+    <br>
+    Status: {{ $fornecedores[1]['status']}}
+    <br>
+    CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Dado não foi preenchido'}}
+@endisset
 
 {{--
 @dd($fornecedores)
@@ -20,17 +27,12 @@
     Unless mensage: Fornecedor Inativo!
 @endunless
 
---}}
-
-@isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome']}}
-    <br>
-    Status: {{ $fornecedores[0]['status']}}
-    <br>
     @isset($fornecedores[0]['cnpj'])
         CNPJ: {{ $fornecedores[0]['cnpj']}}
         @empty($fornecedores[0]['cnpj'])
             Vazio
         @endempty
     @endisset
-@endisset
+
+--}}
+
