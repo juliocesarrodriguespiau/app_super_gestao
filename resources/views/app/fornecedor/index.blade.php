a83693313d35442fd5a0cdc7ad1d272745c8bb44
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2021-12-29 16:07:37 -0300

    commit unless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,3 +1,26 @@
 <h3>Fornecedores</h3>
 
+
+{{--
 @dd($fornecedores)
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados.</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados.</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados.</h3>
+@endif
+--}}
+
+Fornecedor: {{ $fornecedores[0]['nome']}}
+<br>
+Status: {{ $fornecedores[0]['status']}}
+<br>
+@if($fornecedores[0]['status'] == 'N')
+    Fornecedor Inativo!
+@endif
+<br>
+@unless($fornecedores[0]['status'] == 'S')
+    Unless mensage: Fornecedor Inativo!
+@endunless
