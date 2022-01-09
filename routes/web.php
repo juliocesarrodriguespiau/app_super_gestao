449d0bd86af73a8991f735a0ee3be76ae359f49e
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2022-01-08 21:14:47 -0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return 'Olá seja bem-vindo ao curso meu jovem!';
-// });
 
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function (){return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function(){
@@ -30,12 +28,18 @@ Route::prefix('/app')->group(function(){
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
 
+Route::fallback( function(){
+    echo 'A roata acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ira página inicial.';
+});
+
+
+// Route::get('/', function () {
+//     return 'Olá seja bem-vindo ao curso meu jovem!';
+// });
+
 // Route::get('/rota2', function() {
 //     return redirect()->route('site.rota1');
 // })->name('site.rota2');
 
 //Route::redirect('/rota2', 'rota/1');
 
-Route::fallback( function(){
-    echo 'A roata acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ira página inicial.';
-});
